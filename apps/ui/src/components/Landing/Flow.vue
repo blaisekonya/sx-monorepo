dbc4ccc5cbb3511a1874fc16dce016577e2022f6
blaisekonya <blaisekonya@gmail.com>
2024-11-30 09:16:48 +0100

    one more

--- apps/ui/src/components/Landing/Flow.vue
+++ apps/ui/src/components/Landing/Flow.vue
@@ -23,7 +23,7 @@ const STEPS = [
     <UiContainer class="!max-w-screen-lg">
       <div class="pb-6 max-w-[600px] text-center mx-auto">
         <h1 class="mb-4 font-display !text-[36px] sm:!text-[44px]">
-          Addressing the most pressing global challenges
+          A democratic DAO that anyone can join
         </h1>
       </div>
     </UiContainer>
