5d66da98c419930f1ed09de3580b237e05a5c74c
blaisekonya <blaisekonya@gmail.com>
2024-12-04 15:25:09 +0100

    final scroll fix

--- apps/ui/src/components/Site/Topnav.vue
+++ apps/ui/src/components/Site/Topnav.vue
@@ -3,7 +3,7 @@ const { toggleSkin, currentMode } = useUserSkin();
 </script>
 
 <template>
-  <div class="p-4 lg:px-6 flex justify-center items-center z-10 left-0 right-0">
+  <div class="p-4 lg:px-6 flex justify-center items-center absolute z-10 left-0 right-0">
     <div class="grow flex items-center">
       <AppLink :to="{ name: 'site-landing' }" class="inline-block">
         <div class="flex justify-center items-center">
