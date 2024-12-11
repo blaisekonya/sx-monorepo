4e1e75e3a6de68a90ab6df5ee4644196f44629af
blaisekonya <blaisekonya@gmail.com>
2024-12-11 17:47:17 +0100

    bring back sign in modal

--- apps/ui/src/components/Modal/Account.vue
+++ apps/ui/src/components/Modal/Account.vue
@@ -286,10 +286,18 @@ function handleSignup() {
         <UiButton :to="{ name: 'settings-spaces' }" @click="emit('close')">
           Settings
         </UiButton>
-        <UiButton @click="step = 'connect'">
+        <UiButton @click="() => {
+          step = 'connect';
+          showConnectors = true;
+        }
+          ">
           {{ web3.account ? 'Change wallet' : 'Log in' }}
         </UiButton>
-        <UiButton class="!text-skin-danger" @click="handleLogout">
+        <UiButton class="!text-skin-danger" @click="() => {
+          handleLogout();
+          handleLoginClick();
+        }
+          ">
           Log out
         </UiButton>
       </template>
