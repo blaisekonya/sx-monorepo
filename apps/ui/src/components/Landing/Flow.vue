b3b4107cbc4acb73cacde868e63dbd61e02434fd
blaisekonya <blaisekonya@gmail.com>
2024-12-12 01:10:31 +0100

    rewording

--- apps/ui/src/components/Landing/Flow.vue
+++ apps/ui/src/components/Landing/Flow.vue
@@ -23,7 +23,7 @@ const STEPS = [
     <UiContainer class="!max-w-screen-lg">
       <div class="pb-6 max-w-[680px] text-center mx-auto">
         <h1 class="mb-4 font-display text-[36px] xs:text-[44px]">
-          A democratic community that anyone can join
+          Start shaping the future of humanity
         </h1>
       </div>
     </UiContainer>
--- apps/ui/src/components/Landing/Hero.vue
+++ apps/ui/src/components/Landing/Hero.vue
@@ -2,10 +2,10 @@
   <div class="relative flex justify-center min-h-[600px] pt-[94px] items-center border-b">
     <div class="absolute blocks-x top-0 left-[50%] right-0 bottom-0 !bg-left-top border-l" />
     <UiContainer class="relative w-full !max-w-screen-lg flex items-center py-8">
-      <div class="max-w-[640px]">
+      <div class="max-w-[600px]">
         <div class="eyebrow mb-3">World Association</div>
-        <h1 class="mb-5 font-display text-[56px] xs:text-[64px]">
-          Uniting humanity
+        <h1 class="mb-5 font-display text-[52px] xs:text-[60px]">
+          The democratic United Nations alternative
         </h1>
         <UiButton :to="{ name: 'my-home' }" class="primary">
           Get started
