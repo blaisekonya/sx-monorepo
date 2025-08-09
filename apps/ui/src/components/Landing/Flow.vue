a4345c27ac48f97f5359e73384c47613242bac87
blaisekonya <blaisekonya@gmail.com>
2025-08-09 05:52:30 +0200

    fix: update header text to reflect basic income and global democracy

--- apps/ui/src/components/Landing/Flow.vue
+++ apps/ui/src/components/Landing/Flow.vue
@@ -23,7 +23,7 @@ const STEPS = [
     <UiContainer class="!max-w-screen-lg">
       <div class="pb-6 max-w-[800px] text-center mx-auto">
         <h1 class="mb-4 font-display text-[36px] xs:text-[44px]">
-          The home of global democracy
+          Basic income and global democracy
         </h1>
       </div>
     </UiContainer>
