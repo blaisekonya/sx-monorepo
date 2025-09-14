4537e72b5fedb0836543ae2b45c50534ecef4a89
Blaise Konya <blaisekonya@gmail.com>
2025-09-14 18:11:07 +0200

    fix: update citizen and daily UBI claimants counts on landing page for accuracy

--- apps/ui/src/components/Landing/Counter.vue
+++ apps/ui/src/components/Landing/Counter.vue
@@ -3,13 +3,13 @@
     <UiContainer class="!max-w-screen-lg !px-0">
       <div class="grid grid-cols-2 sm:grid-cols-4 text-md ml-0.5">
         <div class="border-r border-b sm:border-b-0 p-4 space-y-2">
-          <h3 class="font-heading !text-[36px]">970k</h3>
+          <h3 class="font-heading !text-[36px]">988k</h3>
           <div class="eyebrow">Citizens</div>
         </div>
         <div
           class="border-r-0 sm:border-r border-b sm:border-b-0 p-4 space-y-2"
         >
-          <h3 class="font-heading !text-[36px]">94k</h3>
+          <h3 class="font-heading !text-[36px]">95k</h3>
           <div class="eyebrow">Daily UBI Claimants</div>
         </div>
         <div class="border-r p-4 space-y-2">
